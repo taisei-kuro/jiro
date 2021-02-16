6c39a1cd563b1449bf9e2bf3e6293d4573cace10
taisei-kuro <t_kurohata@icloud.com>
2021-02-16 21:05:22 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:jiro/next_page.dart';
+import 'package:jiro/main_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: ChangeNotifierProvider(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('jiro'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          ),
+     ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items = ['kboy 1', 'kboy 2', 'kboy 3'];
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KBOYのFlutter大学'),
-      ),
-
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          children: <Widget>[
-
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.orange,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.orange,
-            ),
-          ],
-        )
-
-
-
-      ),
-    );
-     // This trailing comma makes auto-formatting nicer for build methods
-  }
+    );// This trailing comma makes auto-formatting nicer for build methods
+   }
 }
